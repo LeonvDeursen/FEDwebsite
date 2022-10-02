72b45dbb42ac02ba7235ad1a6e19367db9783c61
LeonvDeursen <112863238+LeonvDeursen@users.noreply.github.com>
2022-10-02 17:50:32 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,14 @@
 // JavaScript Document
 console.log("hi");
+
+/*Navigatie Hamburger Menu*/
+
+var navMenu = document.querySelector("nav div:nth-of-type(2) > img");
+
+navMenu.addEventListener("click", openNavMenu);
+
+function openNavMenu() {
+    let moveMenu = document.querySelector("body > ol");
+    moveMenu.classList.toggle("menuOpen");
+    console.log("click")
+}
